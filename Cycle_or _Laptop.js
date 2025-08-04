9cb69175c0f1222d7cc5cd53325b0f2e2242972e
tar1Qul <tariqul.soykot.001.com>
2025-08-04 22:15:10 +0600

    if else condition

---
+++ Cycle_or _Laptop.js
@@ -0,0 +1,12 @@
+var money = 10000;
+var gift;
+
+if (money >= 25000) {
+    gift = "Laptop";
+} else if (money >= 10000) {
+    gift = "Cycle";
+} else {
+    gift = "Chocolate";
+}
+
+console.log(gift);
