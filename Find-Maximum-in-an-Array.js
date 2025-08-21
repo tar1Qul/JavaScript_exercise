ad4b8feff8214d649c43372b808e17e678f64919
MD. TARIQUL ISLAM <148554301+tar1Qul@users.noreply.github.com>
2025-08-21 21:31:01 +0600

    Create Find-Maximum-in-an-Array.js

---
+++ Find-Maximum-in-an-Array.js
@@ -0,0 +1,8 @@
+function findMax(arr) {
+  return Math.max(...arr);
+}
+
+// Test cases
+console.log(findMax([10, 20, 5, 100])); 
+console.log(findMax([-1, -5, -3]));  
+console.log(findMax([7]));  
