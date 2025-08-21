8c2e7ab49ef0b7b7550c15ba31581bb6719bb3dc
MD. TARIQUL ISLAM <148554301+tar1Qul@users.noreply.github.com>
2025-08-21 21:29:09 +0600

    Create Reverse-a-String-using-function.js

---
+++ Reverse-a-String-using-function.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+// Test cases
+console.log(reverseString("hello")); 
+console.log(reverseString("abcd"));  
+console.log(reverseString("a"));     
